0a7d63df535edb1296dfe9f682caed1c5ff31070
LuukasLaitinen23 <98802138+LuukasLaitinen23@users.noreply.github.com>
2026-08-26 12:14:25 +0300

    week 2

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-function greeting() {
-  console.log('Hello World!');
-}
+const registerDialog = document.getElementById('register-dialog');
+const openRegisterDialog = document.getElementById('open-register-dialog');
+const closeRegisterDialog = document.getElementById('close-register-dialog');
+const registerForm = document.getElementById('register-form');
 
-greeting();
+openRegisterDialog.addEventListener('click', () => {
+  registerDialog.showModal();
+});
+
+closeRegisterDialog.addEventListener('click', () => {
+  registerDialog.close();
+});
+
+registerForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  console.log('Account registered');
+
+  registerForm.reset();
+  registerDialog.close();
+});
